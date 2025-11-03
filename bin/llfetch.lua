e810fce06da144e2a56caacbf49fede7586b5288
ZXCTRADEV <168433553+XtraDevgg@users.noreply.github.com>
2025-11-03 15:33:26 +0200

    Create llfetch.lua

---
+++ bin/llfetch.lua
@@ -0,0 +1,21 @@
+local component = require("component")
+local computer = require("computer")
+
+local function get_system_info()
+    return {
+        os = "Krul OS ♱",
+        host = "NachtJäger",
+        uptime = math.floor(computer.uptime() / 3600) .. " hours of suffering",
+        memory = computer.freeMemory() .. "/" .. computer.totalMemory() .. " KB",
+        tier = "Tier " .. computer.tier()
+    }
+end
+
+local info = get_system_info()
+print("┌──────────────────────┐")
+print("│      \27[35mKRUL OS\27[37m       │")
+print("├──────────────────────┤")
+for k, v in pairs(info) do
+    print("│ " .. k .. ": " .. v .. string.rep(" ", 15 - #tostring(v)) .. "│")
+end
+print("└──────────────────────┘")
